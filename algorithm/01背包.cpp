941b62a648fbf0324e4ab1ccb8d36db2f350352b
zst <821988519@qq.com>
2019-09-11 16:29:16 +0800

    Update 01背包.cpp

--- algorithm/01背包.cpp
+++ algorithm/01背包.cpp
@@ -24,19 +24,20 @@ public:
     void bagProblem(int money,vector<int> v,vector<int> h){
         if(money<0)
             return;
-        //申请二维动态数组，dp[i][j]表示到当前第i件物品为止，对于总重量j的结果
+        //申请二维动态数组，dp[i][j]表示到当前第i件物品为止，对于总重价格j的结果
         int dp[v.size()][money+1]={0};
-        //i=0时候要进行初始化，必要
+        //i=0时候要进行初始化，必要；表示只有第一件物品时最大价值是多少
+        //拥有的钱小于第一件物品的时候最大价值为0，拥有价格大于第一件物品的时候最大价值为第一件物品的价值
         for(int j=0;j<=money;j++){
             if(j>=v[0])
                 dp[0][j]=h[0];
         }
-
+        //遍历物品，分每个物品能放下的情况和放不下的情况
         for(size_t i=1;i<v.size();i++){
             for(int j=0;j<=money;j++){
                 if(v[i]>j)//放不下的情况
                     dp[i][j]=dp[i-1][j];
-                else//放的下
+                else//放的下的时候，在之前的钱刚好够放之前的所有物品的情况下加上当前物品的重量，与放之前对比取较大值
                     dp[i][j]=max(dp[i-1][j-v[i]]+h[i],dp[i-1][j]);
             }
         }
